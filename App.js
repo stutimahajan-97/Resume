1d8b163537d1807ddb43bd040bd6950e6ffc3b92
Stuti Mahajan <64328627+stutimahajan-97@users.noreply.github.com>
2021-03-14 22:30:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+import About from './components/about'
+import Education from './components/education'
+
+
+function App() {
+  return (
+    <div className="App">
+        <About></About >
+        <Education></Education>
+    </div>
+  );
+}
+
+
+export default App;
 
