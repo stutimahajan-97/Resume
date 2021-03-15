60c0af5ed2dd3e5f75f6e2492f81cb610c204b96
Stuti Mahajan <64328627+stutimahajan-97@users.noreply.github.com>
2021-03-16 00:27:06 +0530

    Update and rename Education.js to Social.js

---
+++ components/Social.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://github.com/stutimahajan-97" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/stuti-mahajan-740094172/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social 
