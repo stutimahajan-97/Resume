9c7ea46aaea5b4e9e7f0889f2406cc295ff56162
Stuti Mahajan <64328627+stutimahajan-97@users.noreply.github.com>
2021-03-15 15:56:56 +0530

    Update About.js

--- components/About.js
+++ components/About.js
@@ -1,1 +1,15 @@
-
+import React, { Component } from 'react';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email  :  mahajanstuti07@gmail.com</h3>
+          <p>Highly organized and detail-oriented masters graduate from Model Institute of Engineering and technology, University of Jammu.</p>
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
+    
