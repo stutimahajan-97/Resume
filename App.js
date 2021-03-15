bfaea43993de8c1000b9c862862a366952fc2b08
Stuti Mahajan <64328627+stutimahajan-97@users.noreply.github.com>
2021-03-16 00:34:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import './App.css';
-import About from './components/about'
-import Education from './components/education'
+import
+{
+BrowserRouter as Router,
+Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './contents/Home';
+import About from './contents/About';
+import Education from './contents/Education';
+import Skills from './contents/Skills';
+import Contact from './contents/Contact';
+
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-        <About></About >
-        <Education></Education>
+    <Navbar />
+    <Route exact path="/">
+    <Home />
+    </Route>
+    <Route path="/about">
+    <About />
+    </Route>
+    <Route path="/education">
+    <Education />
+    </Route>
+    <Route path="/skills">
+    <Skills />
+    </Route>
+    <Route path="/contact">
+    <Contact />
+    </Route>
     </div>
-  );
-}
-
-
-export default App;
+    </Router>
+    );
+  }
+  
+  export default App;
 
