7c4a0bc688d6dc952cf2965f1f92f1ba9fb0e5ea
Stuti Mahajan <mahajanstuti07@gmail.com>
2021-03-20 23:41:19 +0530

    Create Certification.js

---
+++ contents/Certification.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const Certification = () => {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Certifications</h1>
+            <ul>
+  <li>Certificate for Completion of C language , Advanced Cpp , Java , libreOffice Suit Writer training Spoken Tutorial, IIT BOMBAY</li>
+	<li>Certification for Completion of Career Edge - Knockdown the Lockdown online course offered by   TCS iON</li>
+	<li>Certificate of Participation in Technical Idea Presentation Competition</li>
+	<li>Build a Simple App in Android Studio with Java, Coursera</li>
+	<li>AWS Fundamentals: Going Cloud-Native , Coursera</li>
+	<li>Internet of Things: Communication Technologies ,Coursera </li>
+	<li>Programming for Everybody (Getting Started with Python), Coursera</li>
+    <li>Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning , Coursera.</li>
+	<li>Certificate of Training Programming with Python , Internshala</li>
+	<li>Certificate of Training Web Development, Internshala</li>
+	<li>B and C certified NCC Cadet.</li>
+        </ul>
+            </div>
+            )
+        }
+export default Certification
