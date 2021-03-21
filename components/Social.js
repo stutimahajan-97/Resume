415d5e83f2dfbaaf36dd5cdc93f5394ec52dcf96
Stuti Mahajan <mahajanstuti07@gmail.com>
2021-03-22 02:04:59 +0530

    Update Social.js

--- components/Social.js
+++ components/Social.js
@@ -4,8 +4,8 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-            <a href="https://github.com/stutimahajan-97" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/stuti-mahajan-740094172/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/stutimahajan-97" target="_blank" rel="noopener noreferrer"><i class="fab fa-github">Github</i></a>
+            <a href="https://www.linkedin.com/in/stuti-mahajan-740094172/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in">Linkedin</i></a>
             </div>
             )
         }
